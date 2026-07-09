505c10775aa20805dd87c175195e22977d07ca6c
Tushar Chaturvedi <tusharchaturvedi1909unova@gmail.com>
2026-07-10 03:01:45 +0530

    leetcode 3532

---
+++ Complete DSA/Comparators in C++/r.cpp
@@ -0,0 +1,11 @@
+# include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+    vector<int>a={1,2,3,4,5};
+    reverse(begin(a),end(a));
+    for(int i:a) cout<<i<<" ";
+    cout<<endl;
+    
+ return 0;
+}
---
+++ Complete DSA/LeetCode Daily/Leetcode Daily/3756_Concatenate_Non_Zero_Digits_and_Multiply_by_Sum_II.cpp
@@ -0,0 +1,11 @@
+# include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+    string s = "10203004";
+    string t=s.substr(1,3);
+    cout<<t<<endl;
+
+
+ return 0;
+}
